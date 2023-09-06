a05065187354c8efd8e30af043395b7429e3fcac
olesia-tech <shikotanshop@gmail.com>
2023-09-06 14:32:13 -0400

    added body section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,25 @@
        import HomeHeader from '@/components/HomeHeader'
+import HomeSearch from '@/components/HomeSearch'
+import Image from 'next/image'
 import React from 'react'
        
        export default function Home() {
          return (
           <>
+          {/* header */}
           <HomeHeader />
+
+          {/* body */}
+          <div className='flex flex-col items-center mt-24'>
+            <Image
+            width='300'
+            height='100'
+            src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Google_Images_2015_logo.svg/1200px-Google_Images_2015_logo.svg.png'
+            />
+
+            <HomeSearch />
+          </div>
+
           </>
          )
        }
