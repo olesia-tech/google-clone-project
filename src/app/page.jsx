a3b522b62f43a1b5a3cca7af3155d8b2ce8aaab9
olesia-tech <shikotanshop@gmail.com>
2023-09-07 12:02:34 -0400

    added footer component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -15,7 +15,8 @@ import React from 'react'
             width='300'
             height='100'
             src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Google_Images_2015_logo.svg/1200px-Google_Images_2015_logo.svg.png'
-            />
+            alt='google logo'
+            priority/>
 
             <HomeSearch />
           </div>
---
+++ src/components/CountryLookup.jsx
@@ -0,0 +1,18 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+
+
+//when fetching in client add NEXT_PUBLIC_ to api key(.env.local)
+export default function CountryLookup() {
+    const [country, setCountry] = useState('United States');
+
+    useEffect(() => {
+      fetch(`https://extreme-ip-lookup.com/json/?key=${process.env.NEXT_PUBLIC_IP_API_KEY}`)
+      .then((res) => res.json())
+      .then((data) => setCountry(data.country));
+    },[])
+
+  return (
+    <div>{country}</div>
+  )
+}
