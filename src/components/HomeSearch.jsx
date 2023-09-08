463edb50dfa9455720bcbd7421be02a1c59b1409
olesia-tech <shikotanshop@gmail.com>
2023-09-08 13:25:27 -0400

    completed the search box component

--- src/components/HomeSearch.jsx
+++ src/components/HomeSearch.jsx
@@ -8,24 +8,24 @@ import { BsFillMicFill } from 'react-icons/bs'
 export default function HomeSearch() {
   const router = useRouter();
 const [input, setInput] = useState('');
-const [randomSearchLoading, setRandomSearchLoading] =useState(false);
+const [randomSearchLoading, setRandomSearchLoading] = useState(false);
 
 function handleSubmit(e) {
 e.preventDefault();
 if(!input.trim()) return;
 //router.push handles client-side transitions(next js 13 docs)
 //will redirect user to the new URL 
-router.push('/search/web?searchTerm=${input}');
+router.push(`/search/web?searchTerm=${input}`);
 }
 
 //will redirect user to the new page
 async function randomSearch() {
   setRandomSearchLoading(true);
-const response = await fetch ('https://random-word-api.herokuapp.com/word')
+const response = await fetch ("https://random-word-api.herokuapp.com/word")
 .then((res) => res.json())
 .then((data) => data[0]);
 if(!response) return;
-router.push('/search/web?searchTerm=${response}');
+router.push(`/search/web?searchTerm=${response}`);
 setRandomSearchLoading(false);
 }
 
