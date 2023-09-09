4ce6a1b7d422d7747bd9276475e42a1d1f01c5d1
olesia-tech <shikotanshop@gmail.com>
2023-09-09 12:26:38 -0400

    created search result component

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,7 +1,9 @@
+import WebSearchResults from '@/components/WebSearchResults';
 import Link from 'next/link';
 import React from 'react'
 
 export default async function WebSearchPage({searchParams}) {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
 const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`);
 
 if(!response.ok) {
@@ -28,7 +30,8 @@ if(!results){
 
   return (
     <>
-    {results && results.map((result) => <h1>{result.title}</h1>)}
+    {results && <WebSearchResults results=
+    {data} />}
     </>
   )
 }
