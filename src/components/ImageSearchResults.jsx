37507fe6e474ec811c997f77ef1520353d1b241a
olesia-tech <shikotanshop@gmail.com>
2023-09-11 15:36:51 -0400

    added pagination component

--- src/components/ImageSearchResults.jsx
+++ src/components/ImageSearchResults.jsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
+import PaginationButtons from './PaginationButtons'
 
 export default function ImageSearchResults({results}) {
   return (
@@ -27,6 +28,9 @@ export default function ImageSearchResults({results}) {
            </div>
            </div>
         ))}
+      </div>
+      <div className='ml-16'>
+        <PaginationButtons />
       </div>
     </div>
   )
--- src/components/WebSearchResults.jsx
+++ src/components/WebSearchResults.jsx
@@ -1,12 +1,13 @@
 import Link from 'next/link';
 import React from 'react'
 import Parser from 'html-react-parser'
+import PaginationButtons from './PaginationButtons';
 
 
 export default function WebSearchResults({results}) {
 
   return (
-    <div className='w-full mx-auto px-3 pb-24 sm:pl-[5%] md:pl-[14%] lg:pl-52'>
+    <div className='w-full mx-auto px-3 pb-40 sm:pb-24 sm:pl-[5%] md:pl-[14%] lg:pl-52'>
       {/* display the total number of search results and the time taken for the search */}
      <p
      className='text-gray-600 text-sm mb-5 mt-3'
@@ -34,6 +35,7 @@ export default function WebSearchResults({results}) {
        </p>
       </div>
      ))}
+     <PaginationButtons />
     </div>
   );
 }
