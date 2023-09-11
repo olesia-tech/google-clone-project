71a52df72fffcc3454190b89572b007335359cf3
olesia-tech <shikotanshop@gmail.com>
2023-09-11 16:16:17 -0400

    removed the waiting promise

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -1,10 +1,14 @@
+export const dynamic = 'force-dynamic';
+
 import ImageSearchResults from '@/components/ImageSearchResults';
 import Link from 'next/link';
 import React from 'react'
 
 export default async function ImageSearchPage({searchParams}) {
-  await new Promise((resolve) => setTimeout(resolve, 10000));
-const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}}&searchType=image`);
+const startIndex = searchParams.start || '1';
+
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}}&searchType=image&start=${startIndex}`);
 
 if(!response.ok) {
   throw new Error('Something went wrong');
