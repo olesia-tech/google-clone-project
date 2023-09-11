b3f66e6ee7012ef85a73b419d43ad2d99d6ef1dd
olesia-tech <shikotanshop@gmail.com>
2023-09-11 14:42:50 -0400

    added image search results

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -1,9 +1,38 @@
+import ImageSearchResults from '@/components/ImageSearchResults';
+import Link from 'next/link';
 import React from 'react'
 
-export default function ImageSearchPage () {
+export default async function ImageSearchPage({searchParams}) {
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}}&searchType=image`);
+
+if(!response.ok) {
+  throw new Error('Something went wrong');
+}
+const data = await response.json();
+
+const results = data.items;
+
+if(!results){
   return (
-    <div>
-    ImageSearchPage 
-    </div>
+  <div className='flex flex-col justify-center items-center pt-10'>
+  <h1 className='text-3xl mb-4'>No results found</h1>
+  <p className='text-lg'> Try searching for something else or return to the home page</p>
+  <Link href='/'
+  className='text-blue-500'>
+    Home
+  </Link>
+  
+  </div>
   )
 }
+
+
+  return (
+    <>
+    {results && <ImageSearchResults results=
+    {data} />}
+    </>
+  )
+}
+
